98c790d712b1a98c6fbc17ec9db6881a831bf980
RmanBD <arman0987@gmail.com>
2022-02-04 03:33:44 +0600

    php-wlp folder upload

---
+++ day_14/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'vendor/autoload.php';
+//use App\classes\Example;
+
+
+$myvar = new \App\classes\Example();
+$myvar->index();
+
+
+/*3 rules for declare variable in php
+    i. start with $ sign
+    ii. A-Z, a-z, 0-9, _ ()
+    iii. no numbers in first
+standard rules for variable naming
+    i. start with small latter
+    ii. meaningful
+    iii. readable */
+
